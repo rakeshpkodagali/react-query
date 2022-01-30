cae0ff879953fe56e145ebf8bdf1bdc70b8f66cb
Rakesh Kodagali <rpk.300@gmail.com>
2022-01-30 22:32:17 +0530

    Project Setup.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/pages/Home';
+import SuperHeroes from './components/pages/SuperHeroes';
+import RQSuperHeroes from './components/pages/RQSuperHeroes';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/super-heroes">
+            <SuperHeroes />
+          </Route>
+          <Route path="/rq-super-heroes">
+            <RQSuperHeroes />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/RQSuperHeroes.jsx
@@ -0,0 +1,5 @@
+const RQSuperHeroes = () => {
+  return <h1>RQSuperHeroes</h1>;
+};
+
+export default RQSuperHeroes;
